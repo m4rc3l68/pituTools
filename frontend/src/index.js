887946f50b8621d4a4a91792dffb9bcef7375efd
marcel <marcelinocost69@gmail.com>
2023-01-20 21:04:14 -0300

    commit - 12

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
+import './App.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCheckSquare)
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  document.getElementById('root'),
 )
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
