67efe3c41705b3739fd46945dc3048712e9e1fac
marcel <marcelinocost69@gmail.com>
2023-01-23 19:07:03 -0300

    commit - 18

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,23 +1,43 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Header from '../../components/Header'
+import { Container, InputGroup, FormControl, Button } from 'react-bootstrap'
+import { ContentContainer, Form } from './styles'
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isLoading: false,
+      url: '',
+      shortenedURL: '',
+      errorMenssage: '',
+    }
   }
 
   render() {
     return (
       <Container>
         <Header>Seu novo encurtador de URL. :)</Header>
-        <FontAwesomeIcon icon="check-square" /> PitúTools
+        <ContentContainer>
+          <Form>
+            <InputGroup>
+              <FormControl
+                placeholder="Digite a url para encurtar"
+                defaultValue=""
+                onChange={(e) => this.setState({ url: e.target.value })}
+              />
+              <InputGroup.Append>
+                <Button variant="primary" type="submit">
+                  Encurtar
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+          </Form>
+        </ContentContainer>
       </Container>
     )
   }
 }
 
 export default HomePage
-
-// <FontAwesomeIcon icon="fa-light fa-tree-palm" />
