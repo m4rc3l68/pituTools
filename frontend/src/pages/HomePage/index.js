c3a0993a7d1317c4fbd14f4d01d2a1fe985b66f7
marcel <marcelinocost69@gmail.com>
2023-01-23 10:39:34 -0300

    commit - 16 Bug Corrigido

--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Header from '../../components/Header/'
+import Header from '../../components/Header'
 
 class HomePage extends React.Component {
   constructor(props) {
