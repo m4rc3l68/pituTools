c58a4e35e9e712ff14dc93fdd07040790eb0ab3c
marcel <marcelinocost69@gmail.com>
2023-01-26 19:05:08 -0300

    commit - 26

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,16 +8,16 @@ import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faExclamationTriangle)
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
-)
+) */
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -1,9 +1,11 @@
-/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import Header from '../../components/Header'
 import { Container } from 'react-bootstrap'
 
 import ShortenerService from '../../services/shortenerService'
+
+import { parseISO, formatRelative } from 'date-fns'
+import prBR from 'date-fns/locale/pt-BR'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { StatsContainer, StatsRow, StatsBox, StatsBoxTitle } from './styles'
@@ -16,6 +18,31 @@ class StatsPage extends React.Component {
       isLoading: false,
       shortenedURL: {},
       errorMessage: '',
+    }
+  }
+
+  async componentDidMount() {
+    const { code } = this.props
+
+    try {
+      const service = new ShortenerService()
+      const shortenedURL = await service.getStats(code)
+
+      const parseDate = parseISO(shortenedURL.updateAt)
+      const currentDate = new Date()
+
+      const relativeDate = formatRelative(parseDate, currentDate, {
+        locale: prBR,
+      })
+
+      shortenedURL.relativeDate = relativeDate
+
+      this.setState({ isLoading: false, shortenedURL })
+    } catch (error) {
+      this.setState({
+        isLoading: false,
+        errorMessage: 'Ops... A url solicitada não existe!',
+      })
     }
   }
 
